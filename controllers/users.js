3eea7e88cc5895b4ab8e75c0cc611915acfabd5d
curtlen88 <curtlen88@gmail.com>
2023-02-13 12:35:11 -0700

    user model working

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const db = require('../../models')
+const db = require('../models')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authLockedRoute = require('./authLockedRoute')
